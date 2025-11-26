936c5e7f12bf7e0a0bd60f1e0a48c21a69efddf6
DEV_Romão <brunodossantosromao11@gmail.com>
2025-11-26 12:02:54 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,62 @@
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private int id;
+
+    private String email;
+
+    private String endereco;
+
+    private boolean receberNotificacao;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public boolean isReceberNotificacao() {
+        return receberNotificacao;
+    }
+
+    public void setReceberNotificacao(boolean receberNotificacao) {
+        this.receberNotificacao = receberNotificacao;
+    }
+
+}
