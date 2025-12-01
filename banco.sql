3ea6bcf32ba042079ff722fdcf300f12e66a139e
DEV_Romão <brunodossantosromao11@gmail.com>
2025-12-01 16:41:00 -0300

    banco de dados feito

---
+++ banco.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE ProjetoCrudClient;
+USE ProjetoCrudClient;
+
+CREATE TABLE Cliente(
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(150) NOT NULL,
+    cpf varchar(11) UNIQUE NOT NULL,
+    email varchar(150),
+    endereco varchar(150),
+    receberNT boolean
+);
+
+CREATE TABLE Produto (
+    id_prod int AUTO_INCREMENT PRIMARY KEY,
+    nome_prod varchar(50) UNIQUE NOT NULL,
+    descricao varchar(250),
+    quantidade int,
+    precoUnitario decimal(10,2),
+    fornecedor varchar(100)
+);
+
+CREATE TABLE Admin (
+    id_admin INT PRIMARY KEY,
+    senha VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Admin (id_admin, senha)
+VALUES (1, '1234');
